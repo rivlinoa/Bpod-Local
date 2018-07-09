60c8f58813f32660e826faece03155b92e9aac58
Noa_lab <noa.rivlin@mail.huji.ac.il>
2018-07-09 11:57:28 +0300

    Pharsing RF loop file, added test protocol +tmp file to activate it

--- my/activate protocols/ReadRF_loop.m
+++ my/activate protocols/ReadRF_loop.m
@@ -1,45 +1,49 @@
-%This file is used for running a protocol with RF tag reader on Bpod.
+%% This file is used for running a protocol with RF tag reader on Bpod.
 %User should enter protocol name in the appropriate place. 
 
 %Created by Noa 26.6.18
 
 
 
-
-RFID=serial('COM4'); 
+%% create the RFID object and open it, if it was open - reset and reopen again. 
+instrreset
+clear
+RFID=serial('COM8'); 
 fopen(RFID);
 
-
-%% Define the experiment to run:
+%% open the Bpod object+GUI: 
 Bpod()
 
-experiment='PortAdaptation'; %can be replaced by a fitting function
+%% Define the experiment to run, initiate data folder and graphes. 
+% Load the animals data file - which mice are participating in the experiment, and their tags. 
+experiment='CueAdaptation'; %can be replaced by a fitting function
 load('C:\Users\owner\Documents\Bpod Local\Data\animals_06_27_18.mat');
 prepare_to_protocol('Start', animals);
 
-%TrialStatePlot(AxesHandle, 'init')
 
 %% The main loop for running the protocol 
+%  To stop the loop while running:  ctrl+c,
+%  To resume the same experiment, and data folder after stopping, run this section again.  
 
 while(1)
-%for x= 1:1
+
     tag=fscanf(RFID)
     tag=tag(logical(isstrprop(tag,'digit')+isstrprop(tag,'alpha'))); %eliminate white spaces from the RF read
     if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
         fclose(RFID);
-        global tag
         disp(tag)
-        %CurrentMouseDisplay('init')
-        run_protocol_single_trial(experiment, tag)% possible to add , ['settingsName']
+        run_protocol_single_trial(experiment, tag) % possible to add , ['settingsName']
         fopen(RFID);
     end
     
 end
 
-%RunProtocol('Start', 'FreeAdaptation','0782B19CC86')
-prepare_to_protocol('Stop')
-run_protocol_single_trial('FreeAdaptation', '0782B19CC86')
+%%
 
+prepare_to_protocol('Stop');
+%run_protocol_single_trial('FreeAdaptation', '0782B19CC86')
+
+%% Return lost graphs
 
 %% Delete the RFID object handle
 instrreset
--- my/tmp.m
+++ my/tmp.m
@@ -1,5 +1,11 @@
+
+experiment='Test';
+load('C:\Users\owner\Documents\Bpod Local\Data\animals_06_27_18.mat');
+prepare_to_protocol('Start', animals);
+
+
 tag=cell2mat(animals{1,1})
-for x= 1:2
+for x= 1:5
     
     
     if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
