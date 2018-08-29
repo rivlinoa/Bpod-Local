56480b9670aba845f4b888389d6c32603e3964f7
Noa_lab <noa.rivlin@mail.huji.ac.il>
2018-08-29 16:04:17 +0300

    added probabilities to cue in cloud protocol

--- Protocols/CueInCloud/create_settings.m
+++ Protocols/CueInCloud/create_settings.m
@@ -1,17 +1,19 @@
-%% This file is used to create a settings file for protocol CueAdaptation. 
+%% This file is used to create a settings file for protocol CueAdaptation.
 Settings=struct;
 
 %% user should insert the desired values:
 
 Settings.GUI.RewardAmount = 10; % ul
-Settings.GUI.MinDelay = 0.5; % sec
-S.GUI.ResponseDuration = 3;
+Settings.GUI.ResponseDuration = 3;
 Settings.GUI.MaxDelay = 2; % sec
-Settings.GUI.Delay = ' '; % just for display, don't fill a value. 
+Settings.GUI.MinDelay = 0.5; % sec
+Settings.GUI.LightProb = 0.5; % Between 0-1, fraction of trials that would have auditory+visual stimulus.
+Settings.GUI.CloudProb = 0.5; % Between 0-1, fraction of trials that would have tone cloud during delay .
+Settings.GUI.DifficultyProb = 0.5;% Between 0-1, the proportion of easy trials (bottom 2 attenuations).
 
-%% User should insert a name to describe the settings, in ' ' , and .mat suffix 
-settings_name='default.mat';
+%% User should insert a name to describe the settings, in ' ' , and .mat suffix
+settings_name='allProb0_5.mat';
 
-%% Save the settings 
-file_name=fullfile('C:\Users\owner\Documents\Bpod Local\Protocols\CueInCloud\Settings\', settings_name)
+%% Save the settings
+file_name=fullfile('C:\Users\owner\Documents\Bpod Local\Protocols\Cuego\Settings\', settings_name)
 save(file_name, 'Settings')
--- Protocols/CueInCloud/prepare_to_protocol_CueInCloud.m
+++ Protocols/CueInCloud/prepare_to_protocol_CueInCloud.m
@@ -13,128 +13,93 @@ and initiates the Bpod object.
 % prepare_to_protocol('Stop') - Stops the currently running protocol. Data from the
 % partially completed trial is discarded.
 
-function prepare_to_protocol_CueInCloud(Opstring, animals)
+function prepare_to_protocol_CueInCloud(animals)
+
 global BpodSystem
 
 if isempty(BpodSystem)
     error('You must run Bpod() before launching a protocol.')
 end
-switch Opstring
-    case 'Start'
-        % initiate wave player:
-        
-        if (isfield(BpodSystem.ModuleUSB, 'WavePlayer1'))
-            WavePlayerUSB = BpodSystem.ModuleUSB.WavePlayer1;
-        else
-            error('Error: To run this protocol, you must first pair the AudioPlayer1 module with its USB port. Click the USB config button on the Bpod console.')
-        end
-        A = BpodWavePlayer(WavePlayerUSB);
-        global A
-        
-        % Program sound server
-        A.SamplingRate = 50000; % max in 4 ch configurationn.
-        SF = A.SamplingRate;
-        A.BpodEvents = {'On','On','On','On'};
-        A.TriggerMode = 'Master';
-        A.OutputRange = '0V:5V';
-        load('C:\Users\owner\Documents\Bpod Local\Protocols\CueInCloud\cloud.mat');
-        load('C:\Users\owner\Documents\Bpod Local\Protocols\CueInCloud\cue.mat');
-        cue = cue.*5.*0.99;
-        stim = stim.*5.*0.99;
-        BpodSystem.GUIData.cloud = stim;
-        BpodSystem.GUIData.cue = cue;
-        attenuations = linspace(0.1,1,10);
-        cuemat = attenuations'*cue;
-        cloudmat = attenuations'*stim.*0.1;
-        
-        for i=1:10
-            A.loadWaveform(i, cloudmat(i,:)); % the cloud, for now only one...
-            LoadSerialMessages('WavePlayer1', {['P' ,1, i-1 ]}, i);
-            A.loadWaveform(10+i, cuemat(i,:)); % the cue for now only one ....
-            LoadSerialMessages('WavePlayer1', {['P' ,2, 9+i ]}, 10+i);
-        end
-        LoadSerialMessages('WavePlayer1', {['P',2,0],['P',2,1],['P',2,2],['P',2,3]...
-            ,['P',2,4],['P',2,5],['P',2,6],['P',2,7],['P',2,8],['P',2,9],['P',1,10]...
-            ,['P',1,11],['P',1,12],['P',1,13],['P',1,14],['P',1,15],['P',1,16],['P',1,17]...
-            ['P',1,18],['P',1,19],['S']});
-        
-        %%
-        BpodSystem.Data=struct;
-        BpodSystem.Data.cloud = stim;
-        BpodSystem.Data.cue = cue;
-        % Make standard folders for this experiment.  This will fail silently if the folders exist
-        % define where to save the data from this experiment.
-        formatOut = 'yy.mm.dd_HH.MM.SS';
-        folder_name = datestr(now,formatOut);
-        ExperimentFolder= fullfile(BpodSystem.Path.DataFolder,folder_name);
-        if ~exist(ExperimentFolder)
-            mkdir(ExperimentFolder);
-        end
-        mkdir(fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Data'))
-        mkdir(fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Settings'))
-        FileName = [folder_name '.mat'];
-        DataFolder = fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Data');
-        BpodSystem.Path.DataFolder=ExperimentFolder;
-        
-        % initiate parameters in the Bpood object.
-        BpodSystem.Status.Live = 1;
-        BpodSystem.Path.CurrentDataFile = fullfile(DataFolder, FileName);
-        set(BpodSystem.GUIHandles.RunButton, 'cdata', BpodSystem.GUIData.PauseButton, 'TooltipString', 'Press to pause session');
-        BpodSystem.Status.BeingUsed = 1;
-        BpodSystem.ProtocolStartTime = now*100000;
-        figure(BpodSystem.GUIHandles.MainFig);
-        
-        %BpodNotebook('init'); % Initialize Bpod notebook (for manual data annotation)
-        
-        %% insert animals data into the bpod object for plotting and analysis.
-        
-        BpodSystem.GUIData.animals=struct;
-        BpodSystem.GUIData.animals.animals_names=animals.names;
-        BpodSystem.GUIData.animals.animals_tags=animals.tags;
-        BpodSystem.GUIData.animals.reward_supplied=zeros(size(BpodSystem.GUIData.animals.animals_names));
-        BpodSystem.GUIData.animals.visit_count=zeros(size(BpodSystem.GUIData.animals.animals_names));
-        
-        BpodSystem.GUIData.LastFrequency=0;
-        
-        %% initialize plots:
-        
-        BpodSystem.ProtocolFigures.reward_supplied = figure('name','Reward supplied','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
-        BpodSystem.GUIHandles.reward_supplied = axes();
-        reward_supplied_plot(BpodSystem.GUIHandles.reward_supplied,'init');
-        
-        BpodSystem.ProtocolFigures.visit_count = figure('name','Visit count','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
-        BpodSystem.GUIHandles.visit_count = axes();
-        visit_plot(BpodSystem.GUIHandles.visit_count,'init');
-        
-    case 'Stop'
-        if ~isempty(BpodSystem.Status.CurrentProtocolName)
-            disp(' ')
-            disp([BpodSystem.Status.CurrentProtocolName ' ended.'])
-        end
-        rmpath(fullfile(BpodSystem.Path.ProtocolFolder, BpodSystem.Status.CurrentProtocolName));
-        BpodSystem.Status.BeingUsed = 0;
-        BpodSystem.Status.CurrentProtocolName = '';
-        BpodSystem.Path.Settings = '';
-        BpodSystem.Status.Live = 0;
-        if BpodSystem.EmulatorMode == 0
-            BpodSystem.SerialPort.write('X', 'uint8');
-            pause(.1);
-            nBytes = BpodSystem.SerialPort.bytesAvailable;
-            if nBytes > 0
-                BpodSystem.SerialPort.read(nBytes, 'uint8');
-            end
-            if isfield(BpodSystem.PluginSerialPorts, 'TeensySoundServer')
-                TeensySoundServer('end');
-            end
-        end
-        BpodSystem.Status.InStateMatrix = 0;
-        
-        set(BpodSystem.GUIHandles.RunButton, 'cdata', BpodSystem.GUIData.GoButton, 'TooltipString', 'Launch behavior session');
-        if BpodSystem.Status.Pause == 1
-            BpodSystem.Status.Pause = 0;
-        end
-        % ---- end Shut down Plugins
-        
-        
-        
+
+% initiate wave player:
+if (isfield(BpodSystem.ModuleUSB, 'WavePlayer1'))
+    WavePlayerUSB = BpodSystem.ModuleUSB.WavePlayer1;
+else
+    error('Error: To run this protocol, you must first pair the WavePlayer1 module with its USB port. Click the USB config button on the Bpod console.')
+end
+A = BpodWavePlayer(WavePlayerUSB);
+global A
+
+% Program sound server
+A.SamplingRate = 50000; % max in 4 ch configurationn.
+SF = A.SamplingRate;
+A.BpodEvents = {'On','On','On','On'};
+A.TriggerMode = 'Master';
+A.OutputRange = '0V:5V';
+load('C:\Users\owner\Documents\Bpod Local\Protocols\CueInCloud\cloud.mat');
+load('C:\Users\owner\Documents\Bpod Local\Protocols\CueInCloud\cue.mat');
+cue = cue.*5.*0.99; % maximal rage in 0-5v outputrange
+stim = stim.*(2.5).*0.99;% maximal volume relatively (1:2) in 0-5v outputrange
+
+attenuations = logspace(0,1,10);
+cloudmat = attenuations'*stim.*0.1;
+
+for i=1:10
+    A.loadWaveform(i, cloudmat(i,:)); % the cloud, for now only one with 10 attenuations...
+end
+A.loadWaveform(11, cue); % the cue - maximal volume 
+
+%load serial messages - play cloud in 10 attenuations on channel 2
+%(messages 1-10), play cue on channel 1 (message 11)
+LoadSerialMessages('WavePlayer1', {['P',2,0],['P',2,1],['P',2,2],['P',2,3]...
+    ,['P',2,4],['P',2,5],['P',2,6],['P',2,7],['P',2,8],['P',2,9],['P',1,10],['S']});
+%%
+% Loading the sequences to the data so it would be saved for later
+% analysis. 
+BpodSystem.Data=struct;
+BpodSystem.Data.cloud = stim;
+BpodSystem.Data.cue = cue;
+% Make standard folders for this experiment.  This will fail silently if the folders exist
+% define where to save the data from this experiment.
+formatOut = 'yy.mm.dd_HH.MM.SS';
+folder_name = datestr(now,formatOut);
+ExperimentFolder= fullfile(BpodSystem.Path.DataFolder,folder_name);
+if ~exist(ExperimentFolder)
+    mkdir(ExperimentFolder);
+end
+mkdir(fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Data'))
+mkdir(fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Settings'))
+FileName = [folder_name '.mat'];
+DataFolder = fullfile(BpodSystem.Path.DataFolder,folder_name,'Session Data');
+BpodSystem.Path.DataFolder=ExperimentFolder;
+
+% initiate parameters in the Bpood object.
+BpodSystem.Status.Live = 1;
+BpodSystem.Path.CurrentDataFile = fullfile(DataFolder, FileName);
+set(BpodSystem.GUIHandles.RunButton, 'cdata', BpodSystem.GUIData.PauseButton, 'TooltipString', 'Press to pause session');
+BpodSystem.Status.BeingUsed = 1;
+BpodSystem.ProtocolStartTime = now*100000;
+figure(BpodSystem.GUIHandles.MainFig);
+
+%BpodNotebook('init'); % Initialize Bpod notebook (for manual data annotation)
+
+%% insert animals data into the bpod object for plotting and analysis.
+
+BpodSystem.GUIData.animals=struct;
+BpodSystem.GUIData.animals.animals_names=animals.names;
+BpodSystem.GUIData.animals.animals_tags=animals.tags;
+BpodSystem.GUIData.animals.reward_supplied=zeros(size(BpodSystem.GUIData.animals.animals_names));
+BpodSystem.GUIData.animals.visit_count=zeros(size(BpodSystem.GUIData.animals.animals_names));
+
+%% initialize plots:
+
+BpodSystem.ProtocolFigures.reward_supplied = figure('name','Reward supplied','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
+BpodSystem.GUIHandles.reward_supplied = axes();
+reward_supplied_plot(BpodSystem.GUIHandles.reward_supplied,'init');
+
+BpodSystem.ProtocolFigures.visit_count = figure('name','Visit count','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
+BpodSystem.GUIHandles.visit_count = axes();
+visit_plot(BpodSystem.GUIHandles.visit_count,'init');
+
+
 end
