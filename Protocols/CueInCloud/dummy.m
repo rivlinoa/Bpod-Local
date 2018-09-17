6f900fca8d542a68252afa312fb6a9361b831977
Basement <noa.rivlin@mail.huji.ac.il>
2018-09-17 13:31:01 +0300

    no message

---
+++ Protocols/CueInCloud/dummy.m
@@ -0,0 +1,13 @@
+
+A.loadWaveform(i, cloudmat(i,:)); % the cloud, for now only one...
+A.loadWaveform(10+i, cuemat(i,:)); % the cue for now only one ....
+LoadSerialMessages('WavePlayer1', {['P' ,1, 10 ],['P' ,1, 10 ]});
+LoadSerialMessages('WavePlayer1', ['P' ,1, 10 ], 3);
+            
+sma = NewStateMatrix();
+sma = AddState(sma, 'Name', 'WaitForPresence', ...
+    'Timer', 10,... %what are the units? seconds?
+    'StateChangeConditions', {'Tup', 'exit'},...
+    'OutputActions', {'WavePlayer1', 2});
+SendStateMatrix(sma);
+RawEvents = RunStateMatrix;
