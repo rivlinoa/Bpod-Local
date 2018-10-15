ea364b76c6927f906e5cfe3dbe3972d647858b28
Bpod #2 <noa.rivlin@mail.huji.ac.il>
2018-10-15 09:26:56 +0300

    analysis debugging

--- my/activate protocols/ReadRF_loop.m
+++ my/activate protocols/ReadRF_loop.m
@@ -1,44 +1,43 @@
 %% This file is used for running a protocol with RF tag reader on Bpod.
-% User should enter protocol name in the appropriate place. 
+% User should enter protocol name in the appropriate place.
 
 % Created by Noa 26.6.18
 
 
 
-%% create the RFID object and open it, if it was open - reset and reopen again. 
+%% create the RFID object and open it, if it was open - reset and reopen again.
 instrreset
 clear
-RFID=serial('COM4'); 
+RFID=serial('COM4');
 fopen(RFID);
 
-%% open the Bpod object+GUI: 
-Bpod()
+%% open the Bpod object+GUI:
+Bpod('COM6') 
 
-%% Define the experiment to run, initiate data folder and graphes. 
-% Load the animals data file - which mice are participating in the experiment, and their tags. 
-experiment='CueGo'; %can be replaced by a fitting function
-load('C:\Users\Owner\Documents\Bpod Local\Data\animals_23_07_18.mat')
-% prepare_to_protocol_CueInCloud('Start', animals)
+%% Define the experiment to run, initiate data folder and graphes.
+% Load the animals data file - which mice are participating in the experiment, and their tags.
+experiment='CueGoRig'; %can be replaced by a fitting function
+load('C:\Users\Owner\Documents\Bpod Local\Data\animals_09_06_18.mat')% prepare_to_protocol_CueGo('Start', animals)
+%load('C:\Users\owner\Documents\Bpod Local\Data\animals_09_06_18.mat')% prepare_to_protocol_CueInCloud('Start', animals)
 %cd ('C:\Users\Owner\Documents\Bpod Local\Protocols\CueGo')
 prepare_to_protocol('Start', animals);
 
 
 
-%% The main loop for running the protocol 
+%% The main loop for running the protocol
 %  To stop the loop while running:  ctrl+c,
-%  To resume the same experiment, and data folder after stopping, run this section again.  
+%  To resume the same experiment, and data folder after stopping, run this section again.
 
 while(1)
-
+    
     
     tag=fscanf(RFID)
     tag=tag(logical(isstrprop(tag,'digit')+isstrprop(tag,'alpha'))); %eliminate white spaces from the RF read
-        x=randi(100); %----ADDED 19/08
-     if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
+    if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
         fclose(RFID);
         disp(tag)
-        Ind_Settings=change_settings(tag);
-        run_protocol_single_trial(experiment,tag,Ind_Settings) % possible to add , ['settingsName'] without .mat
+        Ind_Settings = choose_settings_cugorig(tag);
+        run_protocol_single_trial(experiment, tag, Ind_Settings) % possible to add , ['settingsName'] without .mat
         fopen(RFID);
     end
     
