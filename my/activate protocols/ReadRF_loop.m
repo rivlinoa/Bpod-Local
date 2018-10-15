f6d8607b5f9d5223ac5616d9ac4c5715fa1dd0ca
Basement <noa.rivlin@mail.huji.ac.il>
2018-10-15 09:29:11 +0300

    read rf loop mine

--- my/activate protocols/ReadRF_loop.m
+++ my/activate protocols/ReadRF_loop.m
@@ -8,21 +8,18 @@
 %% create the RFID object and open it, if it was open - reset and reopen again. 
 instrreset
 clear
-RFID=serial('COM4'); 
+RFID=serial('COM12'); 
 fopen(RFID);
 
 %% open the Bpod object+GUI: 
-Bpod()
+Bpod('COM11')
 
 %% Define the experiment to run, initiate data folder and graphes. 
 % Load the animals data file - which mice are participating in the experiment, and their tags. 
-experiment='CueGo'; %can be replaced by a fitting function
-load('C:\Users\Owner\Documents\Bpod Local\Data\animals_23_07_18.mat')
-% prepare_to_protocol_CueInCloud('Start', animals)
+experiment='CueInCloud'; %can be replaced by a fitting function
+load('C:\Users\Owner\Documents\Bpod Local\Data\animals_10_03_18.mat')
 %cd ('C:\Users\Owner\Documents\Bpod Local\Protocols\CueGo')
-prepare_to_protocol('Start', animals);
-
-
+prepare_to_protocol_CueInCloud(animals);
 
 %% The main loop for running the protocol 
 %  To stop the loop while running:  ctrl+c,
@@ -30,24 +27,18 @@ prepare_to_protocol('Start', animals);
 
 while(1)
 
-    
     tag=fscanf(RFID)
     tag=tag(logical(isstrprop(tag,'digit')+isstrprop(tag,'alpha'))); %eliminate white spaces from the RF read
-        x=randi(100); %----ADDED 19/08
-     if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
+    if (length(tag)==12) %make sure there wasn't an error in the read (check if valid for all tags!)
         fclose(RFID);
         disp(tag)
-        Ind_Settings=change_settings(tag);
-        run_protocol_single_trial(experiment,tag,Ind_Settings) % possible to add , ['settingsName'] without .mat
+        %Ind_Settings = choose_settings_cloud(tag);
+        run_protocol_single_trial(experiment,tag,'14.10.18') % possible to add , ['settingsName'] without .mat
         fopen(RFID);
     end
     
 end
 
-%%
-
-prepare_to_protocol('Stop');
-%run_protocol_single_trial('FreeAdaptation', '0782B19CC86')
 
 %% Return lost graphs
 
